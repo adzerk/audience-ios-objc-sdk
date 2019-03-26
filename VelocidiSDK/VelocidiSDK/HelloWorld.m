5375053cb81464f7a33cd5167bf4116cba73946b
Duarte Pinto <duartemr.pinto@gmail.com>
2019-03-26 17:46:58 +0000

    Add HelloWorld

---
+++ VelocidiSDK/VelocidiSDK/HelloWorld.m
@@ -0,0 +1,13 @@
+//
+// Created by Duarte Pinto on 2019-03-26.
+// Copyright (c) 2019 Velocidi. All rights reserved.
+//
+
+#import "HelloWorld.h"
+
+
+@implementation HelloWorld
+- (NSString *)hello {
+    return @"Hello, World! \n";
+}
+@end
--- VelocidiSDK/VelocidiSDK/VelocidiSDK.h
+++ VelocidiSDK/VelocidiSDK/VelocidiSDK.h
@@ -1,3 +1,5 @@
+#import <Foundation/Foundation.h>
+
 //! Project version number for VelocidiSDK.
 FOUNDATION_EXPORT double VelocidiSDKVersionNumber;
 
@@ -6,3 +8,4 @@ FOUNDATION_EXPORT const unsigned char VelocidiSDKVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <VelocidiSDK/PublicHeader.h>
 
+#import <HelloWorld.h>
