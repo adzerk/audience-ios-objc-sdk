312da84d9350f4d86151d1ad0432cf9fc70a288b
Duarte Pinto <duartemr.pinto@gmail.com>
2019-03-26 14:59:14 +0000

    Initial project structure

---
+++ README.md
@@ -0,0 +1,5 @@
+# Velocidi iOS SDK in Objective-C 
+
+Velocidi's Objective-C SDK to integrate with iOS apps.
+
+_This is still a very rough draft of the SDK not to ready to be used_
---
+++ VelocidiSDK/VelocidiSDK/VelocidiSDK.h
@@ -0,0 +1,8 @@
+//! Project version number for VelocidiSDK.
+FOUNDATION_EXPORT double VelocidiSDKVersionNumber;
+
+//! Project version string for VelocidiSDK.
+FOUNDATION_EXPORT const unsigned char VelocidiSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VelocidiSDK/PublicHeader.h>
+
