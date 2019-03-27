833226d4f4d2adb94a3254f9f76d402ad2c593c0
Duarte Pinto <duartemr.pinto@gmail.com>
2019-03-27 14:05:36 +0000

    Log test

--- VelocidiSDK/VelocidiSDKTests/VelocidiSDKTests.m
+++ VelocidiSDK/VelocidiSDKTests/VelocidiSDKTests.m
@@ -7,6 +7,7 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "HelloWorld.h"
 
 @interface VelocidiSDKTests : XCTestCase
 
@@ -31,6 +32,7 @@
     // This is an example of a performance test case.
     [self measureBlock:^{
         // Put the code you want to measure the time of here.
+        NSLog([[HelloWorld alloc] init].hello);
     }];
 }
 
