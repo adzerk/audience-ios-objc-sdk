f0eff61c9148d853c17875a88566aac83a573ecf
Duarte Pinto <duartemr.pinto@gmail.com>
2019-03-28 17:31:16 +0000

    Remove Podfile. Fix test

--- VelocidiSDK/VelocidiSDKTests/VelocidiSDKTests.m
+++ VelocidiSDK/VelocidiSDKTests/VelocidiSDKTests.m
@@ -32,7 +32,7 @@
     // This is an example of a performance test case.
     [self measureBlock:^{
         // Put the code you want to measure the time of here.
-        NSLog([[HelloWorld alloc] init].hello);
+        NSLog(@"%@", [[HelloWorld alloc] init].hello);
     }];
 }
 
