7b765fe345b160205393f849df53d8ca43fedc62
Duarte Pinto <duartemr.pinto@gmail.com>
2019-04-12 12:19:24 +0100

    Improve fetching of User-Agent string. Add User-Agent as header to HTTPRequest

--- VelocidiSDK/VelocidiSDK/VSDKRequest.m
+++ VelocidiSDK/VelocidiSDK/VSDKRequest.m
@@ -4,15 +4,45 @@
 @implementation VSDKRequest
 
 + (NSString *)versionedUserAgent {
-    UIDevice *d = [UIDevice currentDevice];
-    return [NSString stringWithFormat:@"%@/%@ %@/%@ (%@; %@/%@)",
-                                      [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleDisplayName"],
-                                      [[NSBundle mainBundle] infoDictionary][@"CFBundleVersion"],
-                                      @"VelocidiSDK",
-                                      @(VelocidiSDKVersionNumber),
-                                      [d model],
-                                      [d systemName],
-                                      [d systemVersion]];
+     NSString *userAgent = nil;
+#if TARGET_OS_IOS
+    // User-Agent Header; see http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.43
+    userAgent = [NSString stringWithFormat:@"%@/%@ %@/%@ (%@; iOS %@; Scale/%0.2f)",
+            [[NSBundle mainBundle] infoDictionary][(__bridge NSString *)kCFBundleExecutableKey] ?: [[NSBundle mainBundle] infoDictionary][(__bridge NSString *)kCFBundleIdentifierKey],
+            [[NSBundle mainBundle] infoDictionary][@"CFBundleShortVersionString"] ?: [[NSBundle mainBundle] infoDictionary][(__bridge NSString *)kCFBundleVersionKey],
+            @"VelocidiSDK",
+            @(VelocidiSDKVersionNumber),
+            [[UIDevice currentDevice] model],
+            [[UIDevice currentDevice] systemVersion],
+            [[UIScreen mainScreen] scale]];
+#elif TARGET_OS_WATCH
+    // User-Agent Header; see http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.43
+    userAgent = [NSString stringWithFormat:@"%@/%@ %@/%@ (%@; watchOS %@; Scale/%0.2f)",
+            [[NSBundle mainBundle] infoDictionary][(__bridge NSString *)kCFBundleExecutableKey] ?: [[NSBundle mainBundle] infoDictionary][(__bridge NSString *)kCFBundleIdentifierKey],
+            [[NSBundle mainBundle] infoDictionary][@"CFBundleShortVersionString"] ?: [[NSBundle mainBundle] infoDictionary][(__bridge NSString *)kCFBundleVersionKey],
+            @"VelocidiSDK",
+            @(VelocidiSDKVersionNumber),
+            [[WKInterfaceDevice currentDevice] model],
+            [[WKInterfaceDevice currentDevice] systemVersion],
+            [[WKInterfaceDevice currentDevice] screenScale]];
+#elif defined(__MAC_OS_X_VERSION_MIN_REQUIRED)
+    userAgent = [NSString stringWithFormat:@"%@/%@ %@/%@ (Mac OS X %@)",
+            [[NSBundle mainBundle] infoDictionary][(__bridge NSString *)kCFBundleExecutableKey] ?: [[NSBundle mainBundle] infoDictionary][(__bridge NSString *)kCFBundleIdentifierKey],
+            [[NSBundle mainBundle] infoDictionary][@"CFBundleShortVersionString"] ?: [[NSBundle mainBundle] infoDictionary][(__bridge NSString *)kCFBundleVersionKey],
+            @"VelocidiSDK",
+            @(VelocidiSDKVersionNumber),
+            [[NSProcessInfo processInfo] operatingSystemVersionString]];
+#endif
+    if (userAgent) {
+        if (![userAgent canBeConvertedToEncoding:NSASCIIStringEncoding]) {
+            NSMutableString *mutableUserAgent = [userAgent mutableCopy];
+            if (CFStringTransform((__bridge CFMutableStringRef)(mutableUserAgent), NULL, (__bridge CFStringRef)@"Any-Latin; Latin-ASCII; [:^ASCII:] Remove", false)) {
+                userAgent = mutableUserAgent;
+            }
+        }
+    }
+    return userAgent;
+
 }
 
 - (id)initWithHTTPSessionManager:(AFHTTPSessionManager *)manager{
@@ -33,7 +63,8 @@
 
 - (void)performRequest {
 
-    NSURLRequest * request = [[AFJSONRequestSerializer serializer] requestWithMethod:@"POST" URLString:self.url.absoluteString parameters:self.data.toDictionary error:nil];
+    NSMutableURLRequest * request = [[AFJSONRequestSerializer serializer] requestWithMethod:@"POST" URLString:self.url.absoluteString parameters:self.data.toDictionary error:nil];
+    [request setValue:VSDKRequest.versionedUserAgent forHTTPHeaderField:@"User-Agent"];
     NSURLSessionDataTask *dataTask = [self.manager
             dataTaskWithRequest:request
             uploadProgress:nil
@@ -46,7 +77,6 @@
                 }
             }];
     [dataTask resume];
-
 }
 
 @end
--- VelocidiSDK/VelocidiSDKTests/SanityTests.m
+++ VelocidiSDK/VelocidiSDKTests/SanityTests.m
@@ -33,16 +33,31 @@
 
 - (void)testUserAgentStr {
     UIDevice *d = [UIDevice currentDevice];
-    bool result = [VSDKRequest.versionedUserAgent
-            isEqualToString:[NSString
-                    stringWithFormat:@"(null)/(null) VelocidiSDK/%@ (%@; %@/%@)",
-                                     @(VelocidiSDKVersionNumber),
-                                     [d model],
-                                     [d systemName],
-                                     [d systemVersion]
-            ]
-    ];
-    XCTAssert(result, @"%@", VSDKRequest.versionedUserAgent);
+    NSString *expectedResult = [NSString
+                                stringWithFormat:@"(null)/(null) VelocidiSDK/%@ (%@; %@ %@; Scale/%0.2f)",
+                                @(VelocidiSDKVersionNumber),
+                                [d model],
+                                [d systemName],
+                                [d systemVersion],
+                                [[UIScreen mainScreen] scale]
+                                ];
+    bool result = [VSDKRequest.versionedUserAgent isEqualToString:expectedResult];
+    XCTAssert(result, @"%@ != %@", expectedResult, VSDKRequest.versionedUserAgent);
+}
+
+- (void)testTrackingRequest {
+    VSDKTrackingEvent * trackingEvent = [[VSDKPageView alloc] init];
+    trackingEvent.clientId = @"0";
+    trackingEvent.siteId = @"1";
+
+    VSDKRequest * request = [[VSDKRequest alloc] init];
+
+    request.data = trackingEvent;
+    request.url = [NSURL URLWithString:@"http://localhost:8080"];
+
+    [request performRequest];
+
+    [NSThread sleepForTimeInterval:5.0f];
 }
 
 @end
