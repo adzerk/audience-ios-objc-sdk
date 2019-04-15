56d60c9efe4d954e42828f88ddd598678f4f4059
Duarte Pinto <duartemr.pinto@gmail.com>
2019-04-15 17:25:32 +0100

    Change workspace to legacy workpsace and add Swift empty test

---
+++ VelocidiSDK/VelocidiSDKTests/NetworkTests.swift
@@ -0,0 +1,15 @@
+import UIKit
+import Quick
+import Nimble
+
+class NetworkTests: QuickSpec {
+    override func spec() {
+        super.spec()
+        
+        describe("first test") {
+            it("should pass") {
+                expect(1).to(equal(1))
+            }
+        }
+    }
+}
