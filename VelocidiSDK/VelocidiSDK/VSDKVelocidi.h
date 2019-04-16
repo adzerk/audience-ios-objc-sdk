ad230efe7d5e9b67ff7d3cbce0e113bb98268e19
Duarte Pinto <duartemr.pinto@gmail.com>
2019-04-16 18:26:49 +0100

    Add doxygen documentation to VSDKVelocidi

--- VelocidiSDK/VelocidiSDK/VSDKVelocidi.h
+++ VelocidiSDK/VelocidiSDK/VSDKVelocidi.h
@@ -11,11 +11,34 @@ NS_ASSUME_NONNULL_BEGIN
 @property (class, readonly, nonatomic) VSDKConfig *config;
 @property (readonly, nonatomic) AFHTTPSessionManager *sessionManager;
 
+/**
+  Method to retrieve the shared instance of the class VSDKVelocidi.
+  This method assumes that the function `.start` as been called before calling this method.
+  Otherwise, it will throw a NSInternalInconsistencyException.
+  @returns shared single instance of the class VSDKVelocidi
+ */
 + (instancetype)sharedInstance;
 
+/**
+  Method that takes the provided configuration and properly initializes the Velocidi SDK according 
+  to the settings of said configuration.
+  @param necessary configuration to properly start the VSDKVelocidi
+  @returns shared single instance of the class VSDKVelocidi
+ */
 + (instancetype) start: (VSDKConfig *)config;
 
+/**
+  Send a request to Velocidi's CDP with a tracking event 
+  @param tracking event to be sent.
+ */
 - (void)track: (VSDKTrackingEvent *) trackingEvent;
+
+/**
+  Send a request to Velocidi's CDP with a tracking event 
+  @param tracking event to be sent.
+  @param Block to be called if the request is successful
+  @param Block to be called if the request is unsuccessful
+ */
 - (void)track: (VSDKTrackingEvent *) trackingEvent
         onSuccess:(void (^)(NSURLResponse *response, id responseObject))onSuccessBlock
         onFailure:(void (^)(NSError * error)) onErrorBlock;
