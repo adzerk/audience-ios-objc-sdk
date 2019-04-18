7f59cecf91aaee08353ff39b676f38833e82f0d3
Duarte Pinto <duartemr.pinto@gmail.com>
2019-04-18 12:09:20 +0100

    Change Podfile structure

--- Pods/Target Support Files/Pods-VelocidiSDK-VelocidiSDKTests/Pods-VelocidiSDK-VelocidiSDKTests-umbrella.h
+++ Pods/Target Support Files/Pods-VelocidiSDK-VelocidiSDKTests/Pods-VelocidiSDK-VelocidiSDKTests-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_VelocidiSDKTestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_VelocidiSDKTestsVersionString[];
+FOUNDATION_EXPORT double Pods_VelocidiSDK_VelocidiSDKTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_VelocidiSDK_VelocidiSDKTestsVersionString[];
 
