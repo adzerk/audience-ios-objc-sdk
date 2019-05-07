018d69122d22a53dfb1c6488f6e524b373e05c47
António Ramadas <antonio_ramadas@hotmail.com>
2019-05-07 12:38:24 +0100

    Update VelocidiSDK/VelocidiSDK/VelocidiSDK.h
    Co-Authored-By: duartepinto <duartemr.pinto@gmail.com>

--- VelocidiSDK/VelocidiSDK/VelocidiSDK.h
+++ VelocidiSDK/VelocidiSDK/VelocidiSDK.h
@@ -6,7 +6,6 @@ FOUNDATION_EXPORT double VelocidiSDKVersionNumber;
 //! Project version string for VelocidiSDK.
 FOUNDATION_EXPORT const unsigned char VelocidiSDKVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <VelocidiSDK/PublicHeader.h>
 
 #import "VSDKAddToCart.h"
 #import "VSDKConfig.h"
