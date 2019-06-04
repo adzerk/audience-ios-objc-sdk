17e0e7618cf43319b83acd79435532202c14029c
Duarte Pinto <duartemr.pinto@gmail.com>
2019-06-04 16:50:44 +0100

    Remove unnecessary test

--- VelocidiSDK/VelocidiSDKTests/SanityTests.m
+++ VelocidiSDK/VelocidiSDKTests/SanityTests.m
@@ -53,29 +53,4 @@
     XCTAssert([@"https://match.domain.com/match" isEqualToString: config.matchUrl.string]);
 }
 
-- (void)testMatchRequest {
-    VSDKUserId * userId1 = [[VSDKUserId alloc] init];
-    userId1.type = @"foo";
-    userId1.userId = @"bar";
-    
-    VSDKUserId * userId2 = [[VSDKUserId alloc] init];
-    userId2.type = @"x";
-    userId2.userId = @"y";
-    
-    NSMutableArray<VSDKUserId *> * uidArray = [[NSMutableArray alloc] init];
-    [uidArray addObject:userId1];
-    [uidArray addObject:userId2];
-
-    VSDKConfig * config = [[VSDKConfig alloc] initWithTrackingHost:@"http://localhost:8080":@"http://localhost:8080"];
-    [[VSDKVelocidi start:config] match:@"baz"
-                               userIds:uidArray
-                             onSuccess:^(NSURLResponse * response, id responseObject) {
-                                 NSLog(@"Success %@", response);
-                             }
-                             onFailure:^(NSError * error){
-                                 NSLog(@"Error %@", error.localizedDescription);
-                             }];
-    [NSThread sleepForTimeInterval:5.0f];
-}
-
 @end
