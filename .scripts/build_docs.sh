f8c26a06def500b0f58f1d41d4f425982aa9e1f8
Duarte Pinto <duartemr.pinto@gmail.com>
2019-07-23 17:18:35 +0100

    Add missing category Setup category to documentation website

--- .scripts/build_docs.sh
+++ .scripts/build_docs.sh
@@ -18,6 +18,7 @@ cp docs/img/* $OUTPUT_DIR/docs/img/
 #Split README into sections
 .scripts/doc-preprocessor.rb README.md "$TMP/readme/README.md" --section "Velocidi iOS SDK in Objective-C" --title "Velocidi iOS SDK in Objective-C"
 .scripts/doc-preprocessor.rb README.md "$TMP/docs/Installation.md" --section "Installation" --title "Installation"
+.scripts/doc-preprocessor.rb README.md "$TMP/docs/Setting up the SDK.md" --section "Setup" --title "Setting up the SDK"
 .scripts/doc-preprocessor.rb README.md "$TMP/docs/Send a track event.md" --section "Send a track event" --title "Send a track event"
 .scripts/doc-preprocessor.rb README.md "$TMP/docs/Make a match.md" --section "Make a match" --title "Make a match"
 # build docs
