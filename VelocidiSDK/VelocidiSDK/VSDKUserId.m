4b0a1fd57e83ff4ce7779962b82ca2dc5b4f457f
André Cardoso <thyandrecardoso@gmail.com>
2021-03-12 11:24:08 +0000

    Force non-empty identifiers and declare a default initializer

--- VelocidiSDK/VelocidiSDK/VSDKUserId.m
+++ VelocidiSDK/VelocidiSDK/VSDKUserId.m
@@ -2,10 +2,31 @@
 
 @implementation VSDKUserId
 
-- (instancetype)initUserId: (NSString *)userId
-                          : (NSString *)type  {
-    if(self = [super init]){
-        self.type = type;
+- (instancetype)init {
+    @throw nil;
+}
+
+- (instancetype)initWithId: (NSString *)userId
+                      type: (NSString *)userIdType {
+    
+    if ([userId length] <= 0) {
+        NSException* ex = [NSException
+                exceptionWithName:@"InvalidArgument"
+                reason:@"id must not be empty!"
+                userInfo:nil];
+        @throw ex;
+    }
+    
+    if ([userIdType length] <= 0) {
+        NSException* ex = [NSException
+                exceptionWithName:@"InvalidArgument"
+                reason:@"type must not be empty!"
+                userInfo:nil];
+        @throw ex;
+    }
+    
+    if (self = [super init]) {
+        self.type = userIdType;
         self.userId = userId;
     }
     return self;
