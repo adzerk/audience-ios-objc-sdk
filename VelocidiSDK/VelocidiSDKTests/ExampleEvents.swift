6594a37399271d6cab0f0ccd403fa02c90a6c251
André Cardoso <thyandrecardoso@gmail.com>
2021-03-17 16:08:54 +0000

    Add tests

---
+++ VelocidiSDK/VelocidiSDKTests/ExampleEvents.swift
@@ -0,0 +1,125 @@
+import Foundation
+import VelocidiSDK
+
+// Event examples taken from https://docs.velocidi.com/collect/events
+struct ExampleEvents {
+    static let appViewJsonStr: String =
+"""
+{
+  "clientId": "velocidi",
+  "siteId": "velocidi.com",
+  "type": "appView",
+  "customFields": {
+    "debug": "true",
+    "role": "superuser",
+  },
+  "title": "Welcome Screen"
+}
+"""
+    static let appViewJsonFoundationObject =  (try? VSDKUtil.tryParseJsonEventString(appViewJsonStr))!
+
+    static let orderPlaceJsonStr =
+"""
+{
+  "clientId": "velocidi",
+  "siteId": "velocidi.com",
+  "type": "orderPlace",
+  "customFields": {
+    "debug": "true",
+    "role": "superuser"
+  },
+  "lineItems": [
+    {
+      "itemGroupId": "p125",
+      "name": "My product",
+      "brand": "Nike",
+      "productType": "Shirts",
+      "currency": "EUR",
+      "promotions": [
+        "WINTERSALE"
+      ],
+      "adult": false,
+      "size": "S",
+      "sizeSystem": "EU",
+      "customFields": {
+        "color": "blue"
+      },
+      "lineItemId": "li125zc",
+      "productId": "p125zc",
+      "total": 102.5,
+      "subtotal": 85,
+      "tax": 11.5,
+      "shipping": 6,
+      "sku": "p125zc-5",
+      "discount": {
+        "value": 5,
+        "currency": "EUR"
+      },
+      "refund": 0,
+      "quantity": 1,
+      "subscriptionDuration": 180
+    }
+  ],
+  "order": {
+    "id": "or123",
+    "currency": "EUR",
+    "total": 102.5,
+    "subtotal": 85,
+    "tax": 11.5,
+    "shipping": 6,
+    "discount": {
+      "value": 5,
+      "currency": "EUR"
+    },
+    "refund": 0,
+    "paymentMethod": "Visa",
+    "shippingMethod": "UPS",
+    "shippingCountry": "France",
+    "promotions": [
+      "WINTERSALE"
+    ]
+  }
+}
+"""
+    static let orderPlaceJsonFoundationObject = (try? VSDKUtil.tryParseJsonEventString(orderPlaceJsonStr))!
+
+    static let productViewJsonStr =
+"""
+{
+  "clientId": "velocidi",
+  "siteId": "velocidi.com",
+  "type": "productView",
+  "customFields": {
+    "debug": "true",
+    "role": "superuser"
+  },
+  "product": {
+    "itemGroupId": "p125",
+    "name": "My product",
+    "brand": "Nike",
+    "productType": "Shirts",
+    "currency": "EUR",
+    "promotions": [
+      "WINTERSALE"
+    ],
+    "adult": false,
+    "size": "S",
+    "sizeSystem": "EU",
+    "customFields": {
+      "color": "blue"
+    },
+    "id": "p125zc",
+    "price": 102.5,
+    "gender": "male"
+  }
+}
+"""
+
+    static let productViewJsonFoundationObject = (try? VSDKUtil.tryParseJsonEventString(productViewJsonStr))!
+
+    static let allEvents = [
+        (appViewJsonStr, appViewJsonFoundationObject),
+        (orderPlaceJsonStr, orderPlaceJsonFoundationObject),
+        (productViewJsonStr, productViewJsonFoundationObject)
+    ]
+}
--- VelocidiSDK/VelocidiSDKTests/VelocidiTests.swift
+++ VelocidiSDK/VelocidiSDKTests/VelocidiTests.swift
@@ -33,100 +33,141 @@ class NetworkTests: QuickSpec {
         describe("VelocidiSDK") {
             let trackURL = "http://tr.testdomain.com"
             let matchURL = "http://match.testdomain.com"
+            let exampleUser1 = VSDKUserId(
+                id: "1c3eae0a556ed83200d7962f72f19961a609e9e59a3551701690f43a13263dc3",
+                type: "email_sha256")
+            let exampleUser2 = VSDKUserId(
+                id: "2c3eae0a556ed83200d7962f72f19961a609e9e59a3551701690f43a13263dc3",
+                type: "email_sha256")
 
             context("test track request") {
+                func httpResponseBuilder(
+                    url: String,
+                    expectedData: [String: AnyHashable]) -> (URLRequest) -> (Response) {
+                    return { (request: URLRequest) in
+                        if request.url!.absoluteString.starts(with: url)
+                            && request.allHTTPHeaderFields?["Content-Type"] == "application/json"
+                            && request.allHTTPHeaderFields?["User-Agent"] != nil
+                            && request.httpBodyStream != nil {
+
+                            let receivedDataFoundationObj =
+                                (try? JSONSerialization.jsonObject(
+                                    with: Data(reading: request.httpBodyStream!),
+                                    options: []) as? [String: AnyHashable])!!
+
+                            if expectedData == receivedDataFoundationObj {
+                                let response = HTTPURLResponse(
+                                    url: request.url!,
+                                    statusCode: 200,
+                                    httpVersion: nil,
+                                    headerFields: nil)!
+                                return .success(response, .noContent)
+                            }
+                        }
+                        return .failure(NSError(domain: trackURL, code: 400))
+                    }
+                }
+
                 it("should successfuly execute tracking requests") {
-                    var responded = false
-                    self.stub({(request: URLRequest) in
-                        return request.url!.absoluteString.starts(with: trackURL)
-                    }, { (request: URLRequest) in
-                        responded = true
-                        let res = HTTPURLResponse(
-                            url: request.url!,
-                            statusCode: 200,
-                            httpVersion: nil,
-                            headerFields: nil)!
-                        return .success(res, .noContent)
-                    })
+                    ExampleEvents.allEvents.forEach { event in
+                        var responded = false
 
-                    let trackingEvent = VSDKPageView()
-                    trackingEvent.siteId = "0"
-                    trackingEvent.clientId = "0"
-                    let userId = VSDKUserId(
-                        id: "1c3eae0a556ed83200d7962f72f19961a609e9e59a3551701690f43a13263dc3",
-                        type: "email_sha256")
-                    let config = VSDKConfig(trackingBaseUrl: trackURL, matchURL)
-                    VSDKVelocidi.start(config!)
-                    VSDKVelocidi.sharedInstance().track(trackingEvent, userId: userId)
+                        // FIXME: Not really sure of the implications of forcing `AnyHashable` here,
+                        //        but it allows comparisons in the tests
+                        let expectedData = (event.1 as? [String: AnyHashable])!
+                        self.stub({(request: URLRequest) in
+                            return request.url!.absoluteString.starts(with: trackURL)
+                        }, { (request: URLRequest) in
+                            responded = true
+                            return httpResponseBuilder(url: trackURL, expectedData: expectedData)(request)
+                        })
 
-                    expect(responded).toEventually(beTrue(), timeout: 4)
+                        let config = VSDKConfig(trackingBaseUrl: trackURL, matchURL)
+
+                        VSDKVelocidi.start(config!)
+                            VSDKVelocidi.sharedInstance().trackJson(event.0, userId: exampleUser1)
+                        expect(responded).toEventually(beTrue(), timeout: 4)
+
+                        responded = false
+                        VSDKVelocidi.start(config!)
+                            VSDKVelocidi.sharedInstance().track(event.1, userId: exampleUser1)
+                        expect(responded).toEventually(beTrue(), timeout: 4)
+                    }
                 }
 
                 it("should successfuly execute tracking requests using success and error handlers") {
-                    func responseBuilder(url: String, expectedData: Data) -> (URLRequest) -> (Response) {
-                        return { (request: URLRequest) in
-                            if request.url!.absoluteString.starts(with: url)
-                                && request.allHTTPHeaderFields?["Content-Type"] == "application/json"
-                                && request.allHTTPHeaderFields?["User-Agent"] != nil
-                                && request.httpBodyStream != nil {
-
-                                let receivedData = Data(reading: request.httpBodyStream!)
-                                if receivedData == expectedData {
-                                    let response = HTTPURLResponse(
-                                        url: request.url!,
-                                        statusCode: 200,
-                                        httpVersion: nil,
-                                        headerFields: nil)!
-                                    return .success(response, .noContent)
-                                }
-                            }
-                            return .failure(NSError(domain: trackURL, code: 400))
-                        }
-                    }
-
-                    func test(success: UnsafeMutablePointer<Bool?>, error: UnsafeMutablePointer<Error?>) {
-                        let trackingEvent = VSDKPageView()
-                        trackingEvent.siteId = "0"
-                        trackingEvent.clientId = "0"
+                    ExampleEvents.allEvents.forEach { event in
+                        // FIXME: Not really sure of the implications of forcing `AnyHashable` here,
+                        //        but it allows comparisons in the tests
+                        let expectedData = (event.1 as? [String: AnyHashable])!
 
                         self.stub({(request: URLRequest) in
                             return request.url!.absoluteString.starts(with: trackURL)
-                        }, responseBuilder(url: trackURL, expectedData: trackingEvent.toJSONData()))
+                        }, httpResponseBuilder(url: trackURL, expectedData: expectedData))
 
                         let config = VSDKConfig(trackingBaseUrl: trackURL, matchURL)
                         VSDKVelocidi.start(config!)
 
-                        let userId = VSDKUserId(
-                            id: "1c3eae0a556ed83200d7962f72f19961a609e9e59a3551701690f43a13263dc3",
-                            type: "email_sha256")
-                        VSDKVelocidi
-                            .sharedInstance()
-                            .track(trackingEvent, userId: userId, onSuccess: { (_: URLResponse, _: Any) in
-                            success.pointee = true
+                        var success = false
+                        var error: Error?
+                        VSDKVelocidi.sharedInstance()
+                            .trackJson(
+                                event.0,
+                                userId: exampleUser1,
+                                onSuccess: { (_: URLResponse, _: Any) in
+                            success = true
                         }, onFailure: {(err: Error) in
-                            success.pointee = false
-                            error.pointee = err
+                            success = false
+                            error = err
                         })
+                        expect(success).toEventually(beTrue(), timeout: 4)
+                        expect(error).to(beNil())
+
+                        success = false
+                        error = nil
+                        VSDKVelocidi.sharedInstance()
+                            .track(
+                                event.1,
+                                userId: exampleUser1,
+                                onSuccess: { (_: URLResponse, _: Any) in
+                            success = true
+                        }, onFailure: {(err: Error) in
+                            success = false
+                            error = err
+                        })
+                        expect(success).toEventually(beTrue(), timeout: 4)
+                        expect(error).to(beNil())
                     }
-
-                    var success: Bool?
-                    var error: Error?
-                    test(success: &success, error: &error)
-
-                    expect(success).toEventually(beTrue(), timeout: 4)
-                    expect(error).to(beNil())
                 }
             }
 
             context("test match requests") {
+                func httpResponseBuilder(url: String, expectedParams: [String: String]) -> (URLRequest) -> (Response) {
+                    return { (request: URLRequest) in
+                        let requestUrl = request.url!.absoluteString
+                        var hasAllParams = true
+                        expectedParams.forEach { (key, value) in
+                            if !requestUrl.contains("\(key)=\(value)") {
+                                hasAllParams = false
+                            }
+                        }
+
+                        if requestUrl.starts(with: url)
+                            && request.allHTTPHeaderFields?["User-Agent"] != nil
+                            && hasAllParams {
+                            let response = HTTPURLResponse(
+                                url: request.url!,
+                                statusCode: 200,
+                                httpVersion: nil,
+                                headerFields: nil)!
+                            return .success(response, .noContent)
+                        }
+                        return .failure(NSError(domain: trackURL, code: 400))
+                    }
+                }
+
                 it("should throw when provider is empty") {
-                    let userId1 = VSDKUserId(
-                        id: "1c3eae0a556ed83200d7962f72f19961a609e9e59a3551701690f43a13263dc3",
-                        type: "email_sha256")
-                    let userId2 = VSDKUserId(
-                        id: "2c3eae0a556ed83200d7962f72f19961a609e9e59a3551701690f43a13263dc3",
-                        type: "email_sha256")
-                    let arrUserIds = NSMutableArray(array: [userId1, userId2])
+                    let arrUserIds = NSMutableArray(array: [exampleUser1, exampleUser2])
 
                     expect(VSDKVelocidi.sharedInstance().match("", userIds: arrUserIds))
                         .to(raiseException(named: "InvalidArgument",
@@ -135,10 +176,7 @@ class NetworkTests: QuickSpec {
                 }
 
                 it("should throw when there are not enough user ids") {
-                    let userId1 = VSDKUserId(
-                        id: "1c3eae0a556ed83200d7962f72f19961a609e9e59a3551701690f43a13263dc3",
-                        type: "email_sha256")
-                    let arrUserIds = NSMutableArray(array: [userId1])
+                    let arrUserIds = NSMutableArray(array: [exampleUser1])
 
                     expect(VSDKVelocidi.sharedInstance().match("baz", userIds: arrUserIds))
                         .to(raiseException(named: "InvalidArgument",
@@ -147,66 +185,29 @@ class NetworkTests: QuickSpec {
                 }
 
                 it("should successfuly execute match requests") {
-                    let userId1 = VSDKUserId(
-                        id: "1c3eae0a556ed83200d7962f72f19961a609e9e59a3551701690f43a13263dc3",
-                        type: "email_sha256")
-                    let userId2 = VSDKUserId(
-                        id: "2c3eae0a556ed83200d7962f72f19961a609e9e59a3551701690f43a13263dc3",
-                        type: "email_sha256")
-
-                    let arrUserIds = NSMutableArray(array: [userId1, userId2])
-
+                    let arrUserIds = NSMutableArray(array: [exampleUser1, exampleUser2])
                     var responded = false
+                    var expectedParams = [String: String]()
+                    expectedParams["cookies"] = "false"
+                    for case let userId as VSDKUserId in arrUserIds {
+                        expectedParams["id_\(userId.type)"] = userId.userId
+                    }
                     self.stub({(request: URLRequest) in
                         return request.url!.absoluteString.starts(with: matchURL)
                     }, { (request: URLRequest) in
                         responded = true
-                        let res = HTTPURLResponse(
-                            url: request.url!,
-                            statusCode: 200,
-                            httpVersion: nil,
-                            headerFields: nil)!
-                        return .success(res, .noContent)
+                        return httpResponseBuilder(url: matchURL, expectedParams: expectedParams)(request)
                     })
 
+                    let config = VSDKConfig(trackingBaseUrl: trackURL, matchURL)
+                    VSDKVelocidi.start(config!)
                     VSDKVelocidi.sharedInstance().match("baz", userIds: arrUserIds)
                     expect(responded).toEventually(beTrue(), timeout: 4)
                 }
 
                 it("should successfuly execute match requests using success and error handlers") {
-                    func  responseBuilder(url: String, expectedParams: [String: String]) -> (URLRequest) -> (Response) {
-                        return { (request: URLRequest) in
-                            let requestUrl = request.url!.absoluteString
-                            var hasAllParams = true
-                            expectedParams.forEach { (key, value) in
-                                if !requestUrl.contains("\(key)=\(value)") {
-                                    hasAllParams = false
-                                }
-                            }
-
-                            if requestUrl.starts(with: url)
-                                && request.allHTTPHeaderFields?["User-Agent"] != nil
-                                && hasAllParams {
-                                let response = HTTPURLResponse(
-                                    url: request.url!,
-                                    statusCode: 200,
-                                    httpVersion: nil,
-                                    headerFields: nil)!
-                                return .success(response, .noContent)
-                            }
-                            return .failure(NSError(domain: trackURL, code: 400))
-                        }
-                    }
-
                     func test(success: UnsafeMutablePointer<Bool?>, error: UnsafeMutablePointer<Error?>) {
-                        let userId1 = VSDKUserId(
-                            id: "1c3eae0a556ed83200d7962f72f19961a609e9e59a3551701690f43a13263dc3",
-                            type: "email_sha256")
-                        let userId2 = VSDKUserId(
-                            id: "2c3eae0a556ed83200d7962f72f19961a609e9e59a3551701690f43a13263dc3",
-                            type: "email_sha256")
-
-                        let arrUserIds = NSMutableArray(array: [userId1, userId2])
+                        let arrUserIds = NSMutableArray(array: [exampleUser1, exampleUser2])
 
                         var expectedParams = [String: String]()
                         expectedParams["cookies"] = "false"
@@ -216,7 +217,7 @@ class NetworkTests: QuickSpec {
 
                         self.stub({(request: URLRequest) in
                             return request.url!.absoluteString.starts(with: matchURL)
-                        }, responseBuilder(url: matchURL, expectedParams: expectedParams))
+                        }, httpResponseBuilder(url: matchURL, expectedParams: expectedParams))
 
                         let config = VSDKConfig(trackingBaseUrl: trackURL, matchURL)
                         VSDKVelocidi.start(config!)
